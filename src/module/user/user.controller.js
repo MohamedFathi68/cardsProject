5ea74673fdbd878b693764b04add0244d4cbfc42
mohamedfathi68 <mohamedfathielqastawy@gmail.com>
2024-11-01 21:17:08 +0200

    س

--- src/module/user/user.controller.js
+++ src/module/user/user.controller.js
@@ -8,14 +8,18 @@ let getAllUsers = async (req, res) => {
 let getUsersByName = async (req, res) => {
   let users = await userModel
     .find({ name: RegExp(req.params.search, "i") })
-    .populate("trader", "-_id -users ").sort({ amount: "asc" , name: "asc" , trader: "asc" });
+    .populate("trader", "-_id -users ")
+    .sort({ amount: "asc", name: "asc", trader: "asc" });
   res.status(200).json({ message: "Success", users });
 };
 
 let getUserByResponsibleName = async (req, res) => {
-  let user = await userModel.find({
-    responsibleName: RegExp(req.params.searchRes, "i")
-  });
+  console.log(req.params.searchRes);
+  
+  let user = await userModel
+    .find({ responsiblePerson: RegExp(req.params.searchRes, "i") })
+    .populate("trader", "-_id -users ")
+    .sort({ amount: "asc", name: "asc", trader: "asc" });
   res.status(200).json({ message: "Success", user });
 };
 
@@ -29,4 +33,10 @@ let deleteUser = async (req, res) => {
   res.status(200).json({ message: "User Deleted", user });
 };
 
-export { getAllUsers, getUsersByName, getUserByResponsibleName , addUser, deleteUser };
+export {
+  getAllUsers,
+  getUsersByName,
+  getUserByResponsibleName,
+  addUser,
+  deleteUser,
+};
